625ee299bbb62b5a98ebab3427be7eaafb7d1eb7
Ryan Wittrup <ryan.wittrup@gmail.com>
2016-10-14 23:33:21 -0400

    create separate routing module

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
 
+import { Router } from '@angular/router';
+
 @Component({
   moduleId: module.id,
   selector: 'my-dashboard',
@@ -12,12 +14,18 @@ import { HeroService } from './hero.service';
 export class DashboardComponent implements OnInit {
   heroes: Hero[] = [];
 
-  constructor(private heroService: HeroService) { }
+  constructor(
+    private router: Router,
+    private heroService: HeroService) { }
+
 
   ngOnInit(): void {
     this.heroService.getHeroes()
       .then(heroes => this.heroes = heroes.slice(1, 5));
   }
 
-  gotoDetail(hero: Hero): void { /* not implements yet */}
+  gotoDetail(hero: Hero): void {
+    let link = ['/detail', hero.id];
+    this.router.navigate(link);
+  }
 }
