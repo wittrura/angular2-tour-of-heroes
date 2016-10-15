9bc8f407a807ff2781d201183a1a865bf5a8a882
Ryan Wittrup <ryan.wittrup@gmail.com>
2016-10-15 00:14:11 -0400

    refactor routing to routing module class

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,20 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
+import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService } from './hero.service';
 
-import { DashboardComponent } from './dashboard.component';
-
-import { routing } from './app.routing';
+import { AppRoutingModule } from './app-routing.module'
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    routing
+    AppRoutingModule
   ],
   declarations: [
     AppComponent,
--- app/app.routing.ts
+++ app/app.routing.ts
@@ -5,25 +5,9 @@ import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 
-const appRoutes: Routes = [
-  {
-    path: '',
-    redirectTo: '/dashboard',
-    pathMatch: 'full'
-  },
-  {
-    path: 'heroes',
-    component: HeroesComponent
-  },
-  {
-    path: 'dashboard',
-    component: DashboardComponent
-  },
-  {
-    path: 'detail/:id',
-    component: HeroDetailComponent
-  }
-
-];
+// const appRoutes: Routes = [
+//
+//
+// ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
